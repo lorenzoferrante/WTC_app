230188936da95033803fb83c2acceb97b38dbc3f
Lorenzo Ferrante <lorenzoferrante96@gmailc.om>
2020-03-02 20:50:48 +0100

    First try commit

--- WTC/ViewController.swift
+++ WTC/ViewController.swift
@@ -8,13 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    // Prova commit
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.title = "WTC"
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
 
 }
+
+// MARK: - UITableViewDataSource
+extension ViewController {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.textLabel?.text = "Cell \(indexPath.row)"
+        
+        return cell
+    }
+    
+}
+
+
 
